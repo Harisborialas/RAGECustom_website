2d17af881c2ee710958f2aa624810abafeeddc94
@Harisborialas <azan6743@gmail.com>
2023-06-06 20:32:31 +0500

    adding carousel

--- src/pages/components/Index.js
+++ src/pages/components/Index.js
@@ -1,9 +1,11 @@
+import Carousel from "./mid_content/carousel/Carousel";
 import Navbar from "./navbar/Navbar";
 
 const Index = () => {
   return (
     <>
       <Navbar />
+      <Carousel/>
     </>
   );
 };
